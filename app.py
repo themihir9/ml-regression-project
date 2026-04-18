d723ea1a1d01a29c6300ee2700fa19faa786e88a
themihir9 <124159688+themihir9@users.noreply.github.com>
2026-04-18 22:28:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
-df = pd.read_csv("solarpowergeneration (2) (1).csv")
+df = pd.read_csv("solarpowergeneration.csv")
 
 
 Q1 = np.percentile(df["distance-to-solar-noon"],25)
